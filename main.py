b5ac024e9d7b70b5bd717600f88025f5ebaf0292
Kushal Goel <kushalgoel786@gmail.com>
2023-07-10 19:57:44 +0530

    Added Homepage and modified public access

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from datetime import date
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app import client
 from app import master
 from app.client_error import ClientError
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"status": "active"}
 
 
 @app.get("/mf/{scheme_code}")
@@ -25,24 +21,6 @@ async def get_scheme_data(scheme_code: str):
         return {"error": e.message}
 
 
-# @app.get("/mf/{scheme_code}/yearly")
-# async def get_yearly_returns(scheme_code: str):
-#     data = await client.fetch_scheme_data(scheme_code)
-#     if "error" not in data.keys():
-#         return master.get_yearly_returns(data["data"])
-#     else:
-#         return data
-
-
-@app.get("/mf/{scheme_code}/nav_history")
-async def get_nav_history(scheme_code: str):
-    try:
-        data = await client.fetch_scheme_data(scheme_code)
-        return data["data"]
-    except ClientError as e:
-        return {"error": e.message}
-
-
 @app.get("/mf/{scheme_code}/nav")
 async def get_nav(scheme_code: str, on: date = date.today()):
     try:
@@ -54,12 +32,34 @@ async def get_nav(scheme_code: str, on: date = date.today()):
         return {"error": e.message}
 
 
-# @app.get("/mf/{scheme_code}/returns")
-# async def get_absolute_returns(scheme_code: str, start: date, end: date = date.today()):
+@app.get("/mf/{scheme_code}/nav_history")
+async def get_nav_history(scheme_code: str):
+    try:
+        data = await client.fetch_scheme_data(scheme_code)
+        return data["data"]
+    except ClientError as e:
+        return {"error": e.message}
+
+
+@app.get("/mf/{scheme_code}/returns")
+async def get_absolute_returns(scheme_code: str, start: date, end: date = date.today()):
+    try:
+        data = await client.fetch_scheme_data(scheme_code)
+        nav_data = data["data"]
+        return master.get_returns(nav_data, start, end)
+    except ClientError as e:
+        return {"error": e.message}
+
+
+app.mount("/", StaticFiles(directory="static", html=True))  # , name="home")
+
+# @app.get("/mf/{scheme_code}/yearly")
+# async def get_yearly_returns(scheme_code: str):
 #     data = await client.fetch_scheme_data(scheme_code)
 #     if "error" not in data.keys():
-#         nav_data = data["data"]
-#         return master.get_returns(nav_data, start, end)
+#         return master.get_yearly_returns(data["data"])
+#     else:
+#         return data
 
 
 # @app.get("/mf/{scheme_code}/absolute")
@@ -76,10 +76,3 @@ async def get_nav(scheme_code: str, on: date = date.today()):
 #     if "error" not in data.keys():
 #         nav_data = data["data"]
 #         return master.get_cagr(nav_data, years)
-
-# TODO
-# List of all schemes
-# error handling
-# enumerate
-# formatting
-# numpy, pandas, matplotlib, flask or django
